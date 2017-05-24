bff43fd792fdf3779405de97f73d70ae71b4eca5
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2017-05-24 11:44:04 +0530

    * set_env.sh: environmental variables.

---
+++ set_env.sh
@@ -0,0 +1,9 @@
+# Created: Monday, May 15 2017
+
+export NETCDF=/usr
+export WRFIO_NCD_LARGE_FILE_SUPPORT=1
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/mpich/lib
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/:$LD_LIBRARY_PATH
+
+export JASPERLIB=/usr/lib/x86_64-linux-gnu
+export JASPERINC=/usr/include
